c8dd89d28c3bc1fb986e23bdba1d202285b83c47
户维娅 <1063201840@qq.com>
2023-02-28 17:09:36 +0800

    feat:到院导航

---
+++ src/components/footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flexa footer-vue">
+    <div v-for="(v, i) in list" :key="i" class="tab-item flexc" @click="activeIndex = i">
+      <div class="fleximg tab-img">
+        <img :src="activeIndex == i ? v.activeImg : v.img" alt="home" />
+      </div>
+      <div :class="activeIndex == i ? 'text-active' : 'text-common'">{{ v.text }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import homeImg from '@/assets/images/home.png'
+import homeActiveImg from '@/assets/images/home_active.png'
+import mineActiveImg from '@/assets/images/mine_active.png'
+import mineImg from '@/assets/images/mine.png'
+
+const activeIndex = ref(0)
+const list = [
+  { text: '首页', img: homeImg, activeImg: homeActiveImg },
+  { text: '个人中心', img: mineImg, activeImg: mineActiveImg },
+]
+
+</script>
+
+<style scoped lang="scss">
+.footer-vue {
+  width: 100%;
+  height: 0.5rem;
+  background: #ffffff;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  .tab-img {
+    width: 0.28rem;
+  }
+  .tab-item {
+    width: 50%;
+    .text-common {
+      font-size: 10px;
+      color: #333333;
+    }
+    .text-active {
+      font-size: 10px;
+      color: #0564f7;
+    }
+  }
+}
+</style>
--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="layout-page">
+  <div class="layout-page" :class="showFooter ? 'main-tab' : ''">
     <el-row class="layout-content">
       <router-view class="router-view"></router-view>
     </el-row>
+    <Footer v-if="showFooter"></Footer>
   </div>
 </template>
 <script setup lang="ts">
-import { ref, getCurrentInstance, ComponentInternalInstance } from 'vue'
+import { ref, computed, getCurrentInstance, ComponentInternalInstance } from 'vue'
+import Footer from '@/components/footer.vue'
+import router from '@/router'
 
 const { proxy } = getCurrentInstance() as ComponentInternalInstance
+const showFooterPaths = ['/home']
+const showFooter = computed(() => showFooterPaths.includes(router.currentRoute._value.path))
 </script>
 <style lang="scss" scoped>
 .layout-page {
   max-width: 750px;
   width: 3.75rem;
   height: 100vh;
-  background: #fafafa;
+  background: #f6f6f6;
+}
+.main-tab {
+  padding-bottom: 0.5rem;
+  overflow: scroll;
 }
 </style>
