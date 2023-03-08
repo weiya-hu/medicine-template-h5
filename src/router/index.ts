543528ffc77f44969ca66c57741231f1ca4bd8f4
户维娅 <1063201840@qq.com>
2023-03-08 16:15:35 +0800

    feat:修改路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -89,8 +89,9 @@ export const routes: RouteRecordRaw[] = [
       // 挂号
       {
         path: '/register',
-        // name: 'Register',
-        // component: () => import('@/views/functional/register.vue'),
+        name: 'Register',
+        component: () => import('@/views/layout/rview.vue'),
+        redirect : '/register',
         children: [
           {
             path: '/register',
